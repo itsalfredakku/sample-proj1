bb646b8246dcb7ec435d11996a1cef60cbd49bb7
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2025-02-06 17:46:03 +0530

    Update

---
+++ backend/Controllers/API/UploadController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers.API;
+
+[ApiController]
+[Route("api/[controller]")]
+public partial class PatientController : Controller
+{
+    
+}
