7f00f02c0b642567c1fc0f74d940f61604b011c4
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2025-02-11 23:48:52 +0530

    Update

---
+++ frontend/src/blocks/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="row-start-1 flex gap-6 flex-wrap items-center justify-center">
+            <a href="/">
+                <h3 className="text-2xl font-bold">Sample Web App</h3>
+            </a>
+        </header>
+    );
+}
